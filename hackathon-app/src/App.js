e3ad05cf3535a0c7119352c5e8e9438a221c4a8b
lucasKimo <kesgoodman7@gmail.com>
2025-08-09 13:39:25 +1000

    edited app.js

--- hackathon-app/src/App.js
+++ hackathon-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Future Scope
+          Welcome to Future Scope!
         </p>
         <a
           className="App-link"
